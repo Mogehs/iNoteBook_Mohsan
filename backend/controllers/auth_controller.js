e0016a32c94e75235f5aa313e0dd5edddedbe778
Mogehs <mugheesurrehman06@gmail.com>
2025-04-26 18:33:13 +0500

    cors errror resolved

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -8,20 +8,26 @@ const register = async (req, res) => {
     if (userExit) {
       res.status(400).send(`massege:email exit already`);
     }
-    const hash_pass= await bcrypt.hash(password,10)
-    
+    const hash_pass = await bcrypt.hash(password, 10);
 
-    
-
-    const CreatedUser = await User.create({ username, email,password:hash_pass, phone });
+    const CreatedUser = await User.create({
+      username,
+      email,
+      password: hash_pass,
+      phone,
+    });
     if (CreatedUser) {
-      res.status(200).json({ massage:CreatedUser, token: await CreatedUser.generateToken() });}
-
+      res
+        .status(200)
+        .json({
+          massage: CreatedUser,
+          token: await CreatedUser.generateToken(),
+        });
+    }
   } catch (error) {
     res.status(400).json(`massege:page not here`, err);
   }
 };
-
 
 const login = async (req, res, next) => {
   try {
@@ -30,19 +36,20 @@ const login = async (req, res, next) => {
     const isUser = await User.findOne({ email });
     if (!isUser) {
       return res.status(401).json({ message: "Invalid Email", field: "email" });
- 
     }
 
     const isPassword = await bcrypt.compare(password, isUser.password);
     if (!isPassword) {
-      return res.status(401).json({ message: "Invalid password" }); 
+      return res.status(401).json({ message: "Invalid password" });
     }
 
     const token = await isUser.generateToken();
 
-    return res.status(200).json({ message: "Login successful", user: isUser, token }); 
+    return res
+      .status(200)
+      .json({ message: "Login successful", user: isUser, token });
   } catch (error) {
-    next(error); 
+    next(error);
   }
 };
 
@@ -55,14 +62,14 @@ const home = async (req, res) => {
     res.status(400).send(`massege:page not here`);
   }
 };
-const user=async (req,res)=>{
-  const userData=req.user
-  
- try {
- return  res.status(200).json({message:userData})
- } catch (error) {
-  console.log("backend error from user")
- }
-}
+const user = async (req, res) => {
+  const userData = req.user;
 
-module.exports = { home, register,login,user };
+  try {
+    return res.status(200).json({ message: userData });
+  } catch (error) {
+    console.log("backend error from user");
+  }
+};
+
+module.exports = { home, register, login, user };
